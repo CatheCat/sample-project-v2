f849b289a0e67f7ccfb00ebd6f87d5f426a14875
Catherine <catherine_chen@cht.com.tw>
2023-08-21 09:27:36 +0800

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
-  <v-app>
-    <app-navigation-drawer></app-navigation-drawer>
-
-    <v-main>
-      <v-container fluid>
-        <!-- ... -->
-      </v-container>
-    </v-main>
-
-    <v-footer>
-      <!-- ... -->
-    </v-footer>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-import AppNavigationDrawer from './components/AppNavigationDrawer.vue'
+#nav {
+  padding: 30px;
 
-export default {
-  name: 'App',
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  components: {
-    AppNavigationDrawer
-  },
-  data: () => ({
-    //...
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
