5825e263c9f9cf656ea0b5a9641541c7c80c17fa
Catherine <catherine_chen@cht.com.tw>
2023-08-28 11:10:48 +0800

    Update plugins

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
+  },
+  lintOnSave: false,
+  transpileDependencies: true,
+  pluginOptions: {
+    vuetify: {}
+  }
 }
